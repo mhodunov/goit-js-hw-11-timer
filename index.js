0fec82cc7e5c19494929c4f50097acce84a829dd
Maksym Hodunov <m.godunov@yandex.ru>
2021-01-11 11:15:25 +0200

    adds solution for part 3

--- index.js
+++ index.js
@@ -2,30 +2,59 @@ class CountdownTimer {
 
   constructor ({selector, targetDate}) {
     
+    this.targetDate = targetDate;
+    // Записываем доступы к соответсвующим элементам на странице
+    this.timer = document.getElementById(selector);
+    this.days = this.timer.querySelector('span[data-value="days"]');
+    this.hours = this.timer.querySelector('span[data-value="hours"]');
+    this.mins = this.timer.querySelector('span[data-value="mins"]');
+    this.secs = this.timer.querySelector('span[data-value="secs"]');
+
+  }
+
+    // Метод, который с интервалом отсчитывает время
+  start() {
+    // Функция обновления элементов на странице вызвана сразу, чтобы не "мелькали" значения прописанные в HTML
+    this.updateTimerOutput();
+
+    setInterval(() => {
+      this.updateTimerOutput();
+    }, 1000);
+  }
+
+  // Функция для обновления значений элментов на странице
+  updateTimerOutput() {
+    let time = this.targetDate - Date.now();
+    const { days, hours, mins, secs } = this.calculateTime(time);
+
+    this.days.textContent = days;
+    this.hours.textContent = hours;
+    this.mins.textContent = mins;
+    this.secs.textContent = secs;
+  }
+
+  // Вспомогательные функции для подсчёта дней, часов и т.д.
+  calculateTime(time) {
+    const days = this.pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+    const hours = this.pad(
+      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+    );
+    const mins = this.pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+    const secs = this.pad(Math.floor((time % (1000 * 60)) / 1000));
+
+    return { days, hours, mins, secs };
+  }
+
+  pad(value) {
+    return String(value).padStart(2, "0");
   }
 }
 
-/*
- * Оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество
- * миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
- */
-const days = Math.floor(time / (1000 * 60 * 60 * 24));
+// Таймер отсчёта до определенной даты
+const newYearCountdown = new CountdownTimer({
+  selector: "timer-1",
+  targetDate: new Date("Dec 31, 2021"),
+});
 
-/*
- * Оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора
- * остатка % и делим его на количество миллисекунд в одном часе
- * (1000 * 60 * 60 = миллисекунды * минуты * секунды)
- */
-const hours = Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-
-/*
- * Оставшиеся минуты: получаем оставшиеся минуты и делим их на количество
- * миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
- */
-const mins = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
-
-/*
- * Оставшиеся секунды: получаем оставшиеся секунды и делим их на количество
- * миллисекунд в одной секунде (1000)
- */
-const secs = Math.floor((time % (1000 * 60)) / 1000);
+// Вызываем таймер
+newYearCountdown.start();
